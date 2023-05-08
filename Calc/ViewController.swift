cda739f7b0a5e3146b62984d6bd86f60b29d329b
Kirill <sayfutdinov-8928@mail.ru>
2023-05-08 13:28:41 +0300

    Initial Commit

---
+++ Calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calc
+//
+//  Created by  Кирилл on 08.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
