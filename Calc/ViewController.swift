a1dea0499297a3fa68e1f6ea310c2bbb3f5a7904
Kirill <sayfutdinov-8928@mail.ru>
2023-05-13 18:33:16 +0300

    Создал функцию для функциональных кнопок

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -9,8 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var numberFromScreen:Double = 0;
+    
+    @IBOutlet weak var Result: UILabel!
+    
     @IBAction func Digits(_ sender: UIButton) {
+        Result.text = Result.text! + String(sender.tag)
+        numberFromScreen = Double(Result.text!)!
     }
+    
+    
+    @IBAction func Buttons(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
     
 
 
