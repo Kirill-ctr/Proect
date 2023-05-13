f99517004873f5f935fab4b2e57109908bc831ac
Kirill <sayfutdinov-8928@mail.ru>
2023-05-13 19:11:04 +0300

    Написал основную логику для кнопок: сложения, вычитания, умножения, деления, очистить и равенство

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -10,16 +10,73 @@ import UIKit
 class ViewController: UIViewController {
     
     var numberFromScreen:Double = 0;
+    var firstNum:Double = 0;
+    var operation:Int = 0;
+    var mathSign:Bool = false;
     
     @IBOutlet weak var Result: UILabel!
     
     @IBAction func Digits(_ sender: UIButton) {
-        Result.text = Result.text! + String(sender.tag)
+        if mathSign == true {
+            Result.text = String(sender.tag)
+            mathSign = false
+        }
+        
+        else{
+            Result.text = Result.text! + String(sender.tag)
+        }
+        
         numberFromScreen = Double(Result.text!)!
     }
     
-    
     @IBAction func Buttons(_ sender: UIButton) {
+        if Result.text != "" && sender.tag != 10 && sender.tag != 15{
+            firstNum = Double(Result.text!)!
+
+            if sender.tag == 11{
+                Result.text = "/"
+            }
+            
+            else if sender.tag == 12{
+                Result.text = "*"
+            }
+            
+            else if sender.tag == 13{
+                Result.text = "-"
+            }
+            
+            else if sender.tag == 14{
+                Result.text = "+"
+            }
+            
+            operation = sender.tag
+            mathSign = true
+        }
+        
+        else if sender.tag == 15{
+            if operation == 11{
+                Result.text = String(firstNum / numberFromScreen)
+            }
+            
+           else if operation == 12{
+                Result.text = String(firstNum * numberFromScreen)
+            }
+            
+            else if operation == 13{
+                 Result.text = String(firstNum - numberFromScreen)
+             }
+            
+            else if operation == 14{
+                 Result.text = String(firstNum + numberFromScreen)
+             }
+        }
+        
+        else if sender.tag == 10{
+            Result.text = ""
+            firstNum = 0
+            numberFromScreen = 0
+            operation = 0
+        }
     }
     
     
